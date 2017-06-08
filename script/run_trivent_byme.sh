5bba4ae57a58264c22565c76442602e234ce13c1
Antoine Pingault <antoine.pingault@gmail.com>
2017-06-08 16:42:25 +0200

    Saving old script

--- script/run_trivent_byme.sh
+++ script/run_trivent_byme.sh
@@ -11,7 +11,6 @@ echo "============= Trivent parameters =================="
 runPeriod="June2016"
 
 RUN=$1
-echo $2
 if [ "$2" == "" ]
 then
     Sync=""
@@ -19,11 +18,15 @@ else
     Sync="_$2"
 fi
 
+# verbosity="MESSAGE"
+verbosity="DEBUG2"
+
 MaxRecordNumber=0
 skipNEvents=0
+eventPrint=10
 
 GAIN_CORRECTION_MODE=false
-electronic_noise_cut=200000
+electronic_noise_cut=175000
 layerGap=2.8
 LayerCut=7
 noiseCut=10
@@ -32,30 +35,40 @@ cerenkovID=3
 cerenkovOffset=0
 cerenkovLength=1
 
+
+if [ ${RUN} -lt 732000 ]; then
+  dataFolder="/Users/antoine/CALICE/DataAnalysis/data" # 2015
+  echo -e "RunNumber : ${RUN} < 732000 -> 2015 data..."
+else 
+  dataFolder="/Volumes/PagosDisk/CALICE/data/SPS_06_2016" #2016
+  echo -e "RunNumber : ${RUN} > 732000 -> 2016 data..."
+fi
+echo -e "\t -> looking for slcio files in $dataFolder"
+
 # dataFolder="/Users/antoine/CALICE/DataAnalysis/data"
-dataFolder="/Volumes/PagosDisk/CALICE/data/SPS_06_2016/StreamOuted"
+# dataFolder="/Volumes/PagosDisk/CALICE/data/SPS_06_2016"
 
 triventFolder="/Users/antoine/CALICE/DataAnalysis/triventArnaud"
 geomFile="${triventFolder}/DifGeom/m3_bonneteau_avril2015.xml"
 
 outputFolder="/Users/antoine/CALICE/DataAnalysis/data"
-outputFile="${outputFolder}/TDHCAL_${RUN}${Sync}" #root/slcio extension added in the xml file
+outputFile="${outputFolder}/TDHCAL_${RUN}${Sync}_Arnaud" #root/slcio extension added in the xml file
 
 
-echo $RUN
-echo $MaxRecordNumber
-echo $GAIN_CORRECTION_MODE
-echo $electronic_noise_cut
-echo $layerGap
-echo $LayerCut
-echo $noiseCut
-echo $timeWin
-echo $cerenkovID
-echo $cerenkovOffset
-echo $cerenkovLength
-echo $dataFolder
-echo $triventFolder
-echo "Sync = " $Sync
+echo -e "MaxRecordNumber : "$MaxRecordNumber
+echo -e "GAIN_CORRECTION_MODE : "$GAIN_CORRECTION_MODE
+echo -e "electronic_noise_cut : "$electronic_noise_cut
+echo -e "layerGap : "$layerGap
+echo -e "LayerCut : "$LayerCut
+echo -e "noiseCut : "$noiseCut
+echo -e "timeWin : "$timeWin
+echo -e "cerenkovID : "$cerenkovID
+echo -e "cerenkovOffset : "$cerenkovOffset
+echo -e "cerenkovLength : "$cerenkovLength
+echo -e "dataFolder : "$dataFolder
+echo -e "triventFolder : "$triventFolder
+echo -e "Sync = " $Sync
+
 echo "================  ILCSoft - ======================="
 
 sourceilc
@@ -69,6 +82,7 @@ if
     [[ $FILELIST == "DHCAL_${RUN}_SO${Sync}.slcio" ]]
 then
     echo $FILELIST
+    echo -e "input files : ${dataFolder}/DHCAL_${RUN}_SO${Sync}.slcio"
 else 
   echo "No Files Found"    
 # else 
@@ -96,7 +110,7 @@ cat >LCIO.xml<<EOF
     <parameter name="LCIOInputFiles">  ${dataFolder}/DHCAL_${RUN}_SO${Sync}.slcio  </parameter>
     <parameter name="MaxRecordNumber">  ${MaxRecordNumber}  </parameter>
     <parameter name="SkipNEvents" value="${skipNEvents}"/>
-    <parameter name="Verbosity" type="string"> MESSAGE </parameter>
+    <parameter name="Verbosity" type="string"> ${verbosity} </parameter>
     <parameter name="SupressCheck" type="bool"> ${GAIN_CORRECTION_MODE} </parameter>
   </global>
   
@@ -110,7 +124,8 @@ cat >LCIO.xml<<EOF
     <parameter name="GAIN_CORRECTION_MODE" type="bool">${GAIN_CORRECTION_MODE} </parameter>
     <parameter name="ElectronicNoiseCut" type="int"> ${electronic_noise_cut} </parameter>
     <parameter name="LayerGap" type="double"> ${layerGap} </parameter>
-    <parameter name="LayerCut" type="int"> ${LayerCut} </parameter>
+    <parameter name="LayerCut" type="int"> ${layerCut} </parameter>
+    <parameter name="HitPerLayerCut" type="int"> ${hitPerLayerCut} </parameter>
     <parameter name="NoiseCut" type="int"> ${noiseCut} </parameter>
     <parameter name="TimeWin" type="int"> ${timeWin} </parameter>
     <parameter name="LCIOOutputFile" value="${outputFile}.slcio"/>
